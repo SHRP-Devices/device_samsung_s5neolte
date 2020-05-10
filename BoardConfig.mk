0e9de3109ea8b97f9d0ce80787e4bf779fe75569
Sherri <shariq.ch@hotmail.com>
2020-05-10 23:25:24 +0500

    Changes for SHRP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,3 +8,27 @@ BOARD_MKBOOTIMG_ARGS += --board SYSMAGIC000KU
 
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+
+#LZMA
+LZMA_RAMDISK_TARGETS := recovery
+LZMA_COMPRESSION := -9
+
+# SHRP flags
+SHRP_PATH := device/samsung/s5neolte
+SHRP_MAINTAINER := Sherri
+SHRP_DEVICE_CODE := s5neolte
+SHRP_EDL_MODE := 0
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+SHRP_FLASH := 1
+SHRP_CUSTOM_FLASHLIGHT := true
+SHRP_FONP_1 := /sys/devices/virtual/camera/flash/rear_flash
+SHRP_FONP_2 := 
+SHRP_FONP_3 := 
+SHRP_FLASH_MAX_BRIGHTNESS := 1
+SHRP_REC := /dev/block/platform/13540000.dwmmc0/by-name/RECOVERY
+SHRP_AB := false
+SHRP_REC_TYPE := Normal
+SHRP_DEVICE_TYPE := A_Only
+SHRP_EXPRESS := true
---
+++ README.md
@@ -0,0 +1,17 @@
+Add to .repo/local_manifests/hero2lte.xml:
+
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+  <project name="shariq342/shrp_device_samsung_s5neolte" path="device/samsung/s5neolte" remote="github" revision="android-8.1" />
+</manifest>
+
+Then run repo sync to check it out.
+
+To build:
+
+export ALLOW_MISSING_DEPENDENCIES=true
+. build/envsetup.sh
+lunch omni_s5neolte-eng
+mka recoveryimage
+
+Kernel sources are available at: https://github.com/TeamWin/android_device_samsung_s5neolte/tree/android-8.1
